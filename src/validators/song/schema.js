7ebfa4db911ffb066449d4101a0f0c9614e7fe1a
notalent2code <rihlanrihlan24@gmail.com>
2023-08-17 14:39:30 +0700

    feat: create song schema for validation

---
+++ src/validators/song/schema.js
@@ -0,0 +1,13 @@
+/* eslint-disable newline-per-chained-call */
+const Joi = require("joi");
+
+const SongSchema = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().integer().required(),
+  genre: Joi.string().required(),
+  performer: Joi.string().required(),
+  duration: Joi.number(),
+  albumId: Joi.string(),
+});
+
+module.exports = { SongSchema };
