0cd96f7d5ab906e87e121a26c95d2cc99b386280
notalent2code <rihlanrihlan24@gmail.com>
2023-08-18 14:24:02 +0700

    feat: create collaboration validator

---
+++ src/validators/collaboration/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require("../../errors/InvariantError");
+const { CollaborationSchema } = require("./schema");
+
+const CollaborationValidator = {
+  validateCollaborationPayload: (payload) => {
+    const validationResult = CollaborationSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = CollaborationValidator;
