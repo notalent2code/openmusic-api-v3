acf75364cfe27a88e1724054a56c9d84b0ea95a0
notalent2code <rihlanrihlan24@gmail.com>
2023-08-24 17:38:56 +0700

    feat: create validator for album cover

--- src/validators/album/index.js
+++ src/validators/album/index.js
@@ -1,9 +1,15 @@
 const InvariantError = require("../../errors/InvariantError");
-const { AlbumSchema } = require("./schema");
+const { AlbumSchema, AlbumCoverSchema } = require("./schema");
 
 const AlbumValidator = {
   validateAlbumPayload: (payload) => {
     const validationResult = AlbumSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateAlbumCoverPayload: (payload) => {
+    const validationResult = AlbumCoverSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
     }
--- src/validators/album/schema.js
+++ src/validators/album/schema.js
@@ -1,8 +1,21 @@
 const Joi = require("joi");
 
 const AlbumSchema = Joi.object({
-    name: Joi.string().required(),
-    year: Joi.number().integer().required(),
+  name: Joi.string().required(),
+  year: Joi.number().integer().required(),
 });
 
-module.exports = { AlbumSchema };
+const AlbumCoverSchema = Joi.object({
+  "content-type": Joi.string()
+    .valid(
+      "image/apng",
+      "image/avif",
+      "image/gif",
+      "image/jpeg",
+      "image/png",
+      "image/webp"
+    )
+    .required(),
+}).unknown();
+
+module.exports = { AlbumSchema, AlbumCoverSchema };
